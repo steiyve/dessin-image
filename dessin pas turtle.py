9f94377bc843ae415cb7bc4a238a278a9b2e29d2
steiyve <94018684+steiyve@users.noreply.github.com>
2023-04-20 14:53:50 -0400

    ma prof est pas contente de la v1

--- dessin pas turtle.py
+++ dessin pas turtle.py
@@ -5,58 +5,39 @@
 import turtle as t
 import random as r
 
-nb_sapin = 1
 
-def sapin(nbSapin):
-    for i in range(nbSapin):
-        t.penup()
-        x=r.randint(-200, 200)
-        y=r.randint(-200, 200)
-        t.goto(x,y)
-        t.pendown()
-        t.fd(40)
+def sapin():
+    for i in range(3):
+        t.fd(10)
         t.left(90)
-        posXY = t.pos()
-        print(posXY)
-        t.fd(100)
+        t.fd(30)
         t.left(90)
-        t.fd(40)
-        t.left(90)
-        t.fd(100)
-        t.left(90)
-        t.penup()
-        x-=40
-        y+=100
-        t.goto(x,y)
-        t.pendown()
+    
+    t.up()
+    t.fd(20)
+    t.down()
+    t.left(-120)
+    for j in range(3):
         for x in range(3):
-            t.fd(120)
-            t.left(120)
-        t.penup()
-        t.goto(x,y)
-        y+=93.23
-        t.goto(x,y)
-        t.pendown()
-        for x in range(3):
-            t.fd(120)
-            
-            t.left(120)
-        t.penup()
-        y+=100
-        t.goto(x,y)
-        t.pendown()
-        for x in range(3):
-            t.fd(120)
-            
-            t.left(120)
-        t.penup()
-        y+=100
-        t.goto(x, y)
-        t.pendown()
+            t.fd(30)
+            t.right(120)
+        
+        t.fd(30)
+        t.up()
+        t.left(120)
+        t.fd(15)
+        t.down()
+        t.right(120)
+    
+    t.right(60)
 
+nb_sapin = 4
+for i in range(nb_sapin):
+    t.up()
+    t.goto(r.randint(-300,300), r.randint(-300,300))
+    t.down()
+    sapin()
 
-
-sapin(nb_sapin)
 
 while True:
     pass
